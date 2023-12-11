d084885b6df8b399a247b8d41809d6fb370eaeac
Andrea Escudero <7460086@alu.murciaeduca.es>
2023-12-11 09:03:44 +0100

    Probando los datos de entrada

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Estudiante;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class EstudianteController extends Controller
 {
 
-    public function getIndex(){
-        return view('estudiantes.index',['arrayEstudiantes' => Estudiante::all()]);
+    public function getIndex()
+    {
+        return view('estudiantes.index', ['arrayEstudiantes' => Estudiante::all()]);
     }
 
     public function getShow($id)
@@ -18,17 +20,33 @@ class EstudianteController extends Controller
             ->with('estudiante', Estudiante::findOrFail($id));
     }
 
-    public function getEdit($id) {
+    public function getEdit($id)
+    {
         return view('estudiantes.edit')
             ->with("estudiante", Estudiante::findOrFail($id));
     }
 
-    public function putEdit($id) {
-        return view('estudiantes.edit')
-            ->with("estudiante", Estudiante::findOrFail($id));
+    public function putEdit(Request $request, $id)
+    {
+        $estudiante = Estudiante::findOrFail($id);
+
+        // TODO: Eliminar el avatar anterior si existiera
+        $path = $request->file('avatar')->store('avatars', ['disk' => 'public']);
+        $estudiante->avatar = $path;
+        $estudiante->save();
+
+        $estudiante->update($request->all());
+        return redirect()->action([self::class, 'getShow'], ['id' => $estudiante->id]);
     }
 
-    public function getCreate(){
+    public function getCreate()
+    {
         return view('estudiantes.create');
+    }
+
+    public function store(Request $request)
+    {
+        $estudiante = Estudiante::create($request->all());
+        return redirect(action([self::class, 'getShow'], ['id' => $estudiante->id]));
     }
 }
